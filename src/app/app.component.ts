e463810c27c525ead90c57cac921158864c0b188
satya <kurupudisatyadevi2005@gmail.com>
2024-10-05 15:27:02 +0530

    "First angular program"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo';
+  image=[
+    {
+      "src":"https://images.pexels.com/photos/28651765/pexels-photo-28651765/free-photo-of-serene-water-lily-bloom-on-peaceful-lake.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+      "alt":"imgage1",
+      "title":"",
+      "description":"",
+    },
+    {
+      "src":"https://images.pexels.com/photos/20218579/pexels-photo-20218579/free-photo-of-a-woman-pouring-coffee-from-a-moka-pot-into-a-cup.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+      "alt":"imgage1",
+      "title":"",
+      "description":"",
+    },
+  ]
 }
